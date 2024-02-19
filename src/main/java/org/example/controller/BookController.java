6155e26714001a04001e845db32ebc56e475e995
adithya2233 <adithyanethmini2006@gmail.com>
2024-02-19 18:28:53 +0530

    add book method completed

--- src/main/java/org/example/controller/BookController.java
+++ src/main/java/org/example/controller/BookController.java
@@ -3,6 +3,8 @@ package org.example.controller;
 import org.example.dto.Book;
 import org.example.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
     @Autowired
     BookService service;
-    public void addBook(Book book){
-
+    @PostMapping
+    public void addBook(@RequestBody Book book){
+        service.addBook(book);
     }
 }
